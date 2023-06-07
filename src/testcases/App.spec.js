c25dbf0e598ebcb18548c60ca5d83e75f51d4376
Nisha <nkolivia1234@gmail.com>
2023-06-07 16:03:39 +0530

    Refactor:updated folder structure.

--- src/testcases/App.spec.js
+++ src/testcases/App.spec.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../components/App';
 
 it("should have the app loaded", () => {
   render(<App/>)
