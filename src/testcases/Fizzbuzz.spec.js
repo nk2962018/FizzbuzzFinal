b75a51b0767f607b925efbd45a6a45ab8d9ad323
Nisha <nkolivia1234@gmail.com>
2023-06-08 11:36:34 +0530

    Refactor:group similar tasks in fizzbuzz test file.

--- src/testcases/Fizzbuzz.spec.js
+++ src/testcases/Fizzbuzz.spec.js
@@ -2,19 +2,16 @@ import Fizzbuzz from "../components/Fizzbuzz";
 import { render, screen, fireEvent } from "@testing-library/react";
 
 describe("The Fizzbuzz component works fine when", () => {
-
-    it("should have the fizzbuzz loaded", () => {
-    render(<Fizzbuzz/>);
+    beforeEach(() => {
+        render(<Fizzbuzz/>);
     });
 
     it("should have the increment button loaded", () => {
-        render(<Fizzbuzz/>);
         const incrementButton = screen.getByTestId("Increment");
         expect(incrementButton).toBeInTheDocument();
     });
-    
+
     it("should have the decrement button loaded", () => {
-        render(<Fizzbuzz/>);
         const incrementButton = screen.getByTestId("Decrement");
         expect(incrementButton).toBeInTheDocument();
     });
