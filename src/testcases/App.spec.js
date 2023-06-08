7ae714f2216439dc65642912e2e4e8b7dd6939e7
Nisha <nkolivia1234@gmail.com>
2023-06-08 10:53:28 +0530

    Refactor:group similar tasks in testfiles.

--- src/testcases/App.spec.js
+++ src/testcases/App.spec.js
@@ -1,28 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from '../components/App';
 
-it("should have the app loaded", () => {
-  render(<App/>)
-  const appComponent = screen.getByTestId("parent");
-  expect(appComponent).toBeInTheDocument();
+describe("The App works fine when", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+  it("should have the app loaded", () => {
+    const appComponent = screen.getByTestId("parent");
+    expect(appComponent).toBeInTheDocument();
+  });
+
+  it("should have the header loaded", () => {
+    const header = screen.getByTestId("header");
+    expect(header).toBeInTheDocument();
+  }); 
+
+  it("should have the proper header name", () => {
+    const header = screen.getByTestId("header");
+    expect(header).toHaveTextContent("The Fizzbuzz Program");
+  });
+
+  it("should have the fizzBuzz loaded", () => {
+    const fizzBuzz = screen.getByTestId("fizzBuzz");
+    expect(fizzBuzz).toBeInTheDocument();
+  });
+
 });
-
-it("should have the header loaded", async () => {
-  render(<App/>)
-  const header = screen.getByTestId("header");
-  expect(header).toBeInTheDocument();
-}); 
-
-it("should have the proper header name", async () => {
-  render(<App/>)
-  const header = screen.getByTestId("header");
-  expect(header).toHaveTextContent("The Fizzbuzz Program");
-});
-
-it("should have the fizzBuzz loaded", () => {
-  render(<App/>)
-  const fizzBuzz = screen.getByTestId("fizzBuzz");
-  expect(fizzBuzz).toBeInTheDocument();
-});
-
-
--- src/testcases/Fizzbuzz.spec.js
+++ src/testcases/Fizzbuzz.spec.js
@@ -1,7 +1,6 @@
 import Fizzbuzz from "../components/Fizzbuzz";
 import { render, screen, fireEvent } from "@testing-library/react";
 
-
 describe("The Fizzbuzz component works fine when", () => {
 
     it("should have the fizzbuzz loaded", () => {
